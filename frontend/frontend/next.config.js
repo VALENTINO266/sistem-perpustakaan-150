c0373660e0f49694a045c86b71ba4585285233ee
Candra Valentino <2200016023@webmail.uad.ac.id>
2026-07-17 21:05:26 +0700

    Update next config for vercel

--- frontend/frontend/next.config.js
+++ frontend/frontend/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-};
+  output: 'standalone', // Ini wajib supaya Vercel mengerti ini Next.js
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
